ac0b0a97686c17953ba4df370e2c0d780b70c046
jd <jean.daniel.michaud@gmail.com>
2021-10-16 14:32:59 +0200

    Export Dicom objects.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 import { DICOMWebDataset } from './dicom-web-dataset';
 import { DICOMWebResponse, Element } from './dicom-web-response';
 export { DicomFactory } from './dicom-factory';
+export { Study } from './study';
+export { Series } from './series';
+export { Instance } from './instance';
+export { Frames } from './frames';
